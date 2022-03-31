dd629118c8999028996f063ca5ce3275a87ff5d9
nestorclash <101343323+nestorclash@users.noreply.github.com>
2022-03-31 14:58:04 -0600

    Add files via upload

---
+++ Demo_ejemplo.java
@@ -0,0 +1,16 @@
+
+package Demostracion;
+
+import java.util.Scanner;
+
+
+public class Demo_ejemplo {
+    public static void main(String[] args) {
+        
+        Scanner leer=new Scanner(System.in);
+        Estudiante objeto=new Estudiante();
+        
+       
+    }
+    
+}
---
+++ Estudiante.java
@@ -0,0 +1,64 @@
+
+package Demostracion;
+
+public class Estudiante {
+    int edad=0;
+    String nombres;
+    String apellidos;
+    String direccion;
+    int carnet;
+    
+  
+    
+    
+    public Estudiante(){
+        
+    }
+
+    public Estudiante(String nombres, String apellidos) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+    }
+
+    public Estudiante(String nombres, String apellidos, String direccion, int carnet) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.carnet = carnet;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(int carnet) {
+        this.carnet = carnet;
+    }
+    
+    
+    
+}
