3eeff8c46531dc361bd43e4a27bf83ef81d1201a
nestorclash <101343323+nestorclash@users.noreply.github.com>
2022-04-01 16:47:54 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,15 @@
+package cliente;
+
+public class Cliente {
+    
+    String duiCliente, nombreCliente;
+    long telefonoCliente;
+    
+    
+    public Cliente (String duiCliente, String nombreCliente, long telefonoCliente){
+        this.duiCliente=duiCliente;
+        this.nombreCliente=nombreCliente;
+        this.telefonoCliente=telefonoCliente;
+    }
+    
+}
